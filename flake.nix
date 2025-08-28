a9e05afaccd5d5bf49c45e4b1c33bda56c92482c
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-08-27 18:31:05 -0700

    remove flake parts as we cannot build for linux from darwin

--- flake.nix
+++ flake.nix
@@ -2,44 +2,27 @@
   description = "proxmox + nixos vms flake";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     terranix = {
       url = "github:terranix/terranix";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    flake-parts,
-    nixpkgs,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      perSystem = {system, ...}: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        formatter = pkgs.writeShellApplication {
-          name = "format";
-          runtimeInputs = builtins.attrValues {
-            inherit (pkgs) alejandra deadnix fd statix;
-          };
-          text = ''
-            fd "$@" -t f -e nix -x statix fix -- '{}'
-            fd "$@" -t f -e nix -X deadnix -e -- '{}'
-            fd "$@" -t f -e nix -X alejandra '{}'
-          '';
-        };
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    formatter.${system} = pkgs.writeShellApplication {
+      name = "format";
+      runtimeInputs = builtins.attrValues {
+        inherit (pkgs) alejandra deadnix fd statix;
       };
+      text = ''
+        fd "$@" -t f -e nix -x statix fix -- '{}'
+        fd "$@" -t f -e nix -X deadnix -e -- '{}'
+        fd "$@" -t f -e nix -X alejandra '{}'
+      '';
     };
+  };
 }
