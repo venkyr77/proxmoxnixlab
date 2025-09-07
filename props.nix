e84f14cc352b720ad0f365d77690d140c12045b3
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-09-06 19:28:01 -0700

    add searx

--- props.nix
+++ props.nix
@@ -102,5 +102,14 @@
       };
       vm_id = 160;
     };
+    searx = {
+      cpu_cores = 1;
+      disk_size = 16;
+      ipv4_full = "10.0.0.161/24";
+      ipv4_short = "10.0.0.161";
+      memory = 1 * 1024;
+      mount_point = null;
+      vm_id = 161;
+    };
   };
 }
