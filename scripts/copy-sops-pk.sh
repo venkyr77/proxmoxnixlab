8b570b181b4f1fb57b3d69e5919ee85ecb2504c0
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-10-06 04:05:43 -0700

    fixes

--- scripts/copy-sops-pk.sh
+++ scripts/copy-sops-pk.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 read -r -p "Enter host ip: " host
 
 ssh -t "ops@$host" 'sudo mkdir -p "/etc/$HOSTNAME"'
-scp "$HOME/.config/sops/age/keys.txt" "ops@$host:'$HOME/sopspk'"
+scp "$HOME/.config/sops/age/keys.txt" "ops@$host:/home/ops/sopspk"
 ssh -t "ops@$host" 'sudo mv "$HOME/sopspk" "/etc/$HOSTNAME"'
